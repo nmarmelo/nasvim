8b1a928355bdce89917ab4e2e685203b75fe723a
nmarmelo <nathanmarmelo@hotmail.com>
2025-05-20 18:00:12 -0400

    Enable simple mason-lspconfig setup

--- lua/nasmarr/lazy/lsp.lua
+++ lua/nasmarr/lazy/lsp.lua
@@ -40,6 +40,7 @@ return {
 
         require("fidget").setup({})
         require("mason").setup()
+        require("mason-lspconfig").setup()
 --         require("mason-lspconfig").setup({
 --             ensure_installed = {
 --                 "lua_ls",
