74ff39066b10f7a007431dcef01d2ddb16ca5c9f
nmarmelo <nathanmarmelo@hotmail.com>
2025-05-30 00:16:33 -0400

    Removed pinned version from mason

--- lua/nasmarr/lazy/mason.lua
+++ lua/nasmarr/lazy/mason.lua
@@ -1,12 +1,10 @@
 return {
     {
         "mason-org/mason.nvim",
-        version = "1.8.3",
         opts = {},
     },
     {
         "mason-org/mason-lspconfig.nvim",
-        version = "1.21.0",
         dependencies = { "neovim/nvim-lspconfig" },
         opts ={},
     },
